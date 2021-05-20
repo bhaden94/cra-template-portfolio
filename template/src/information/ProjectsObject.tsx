ddd52610df6cb2a7c69cf0fef6ed863319abf533
bhaden94 <bhaden94@yahoo.com>
2021-05-20 18:47:14 -0400

    projects section filler information one

--- template/src/information/ProjectsObject.tsx
+++ template/src/information/ProjectsObject.tsx
@@ -1,35 +1,33 @@
-import covid from "../images/projects/covid-app.png";
-import spacex from "../images/projects/spacex-app.png";
-import {
-	ITechnologiesObject,
-	TechnologiesObject
-} from "./TechnologiesObject";
+import { ITechnologiesObject, TechnologiesObject } from "./TechnologiesObject";
+
+/* Project image import section */
+import Google from "../images/projects/google-clone.png";
+import Netflix from "../images/projects/netflix-clone.png";
 
 export interface IProjectObject {
-	id: number;
 	media: string;
 	title: string;
 	shortDesc: string; // max 125 characters long, everything else will be cutoff in card
 	accomplishments: string[]; // This can be as many bullets as you want, but will show a scroll bar if necesary
-	liveLink?: string;
-	codeLink?: string;
-	techUsed?: ITechnologiesObject[]; // max 9 techologies here for proper display on card
+	liveLink?: string; // optional
+	codeLink?: string; // optional
+	techUsed?: ITechnologiesObject[]; // max 9 techologies here for proper display on card. Also optional
 }
 
 /*
 	Put your projects in here to be displayed in a grid on the page
-
-	The id field should be unique
-
-	the media field needs to be imported at the top first and then use that as the media object
-		note: the pictures should be stored in src/images/projects
 	
 	The accomplishments field can be as many bullets as you want, but there will be a scroll
-	bar if needed. 
+	bar if needed. If you want more room for accomplishments, then remove the techUsed section.
 
 	The techUsed field will be limited to the first 9 technologies.
 	This is so we don't overflow the card on some screen sizes
 		note: go to TechnologiesObj.tsx to modify the list of technologies that are available for use
+
+	Optional fields:
+		liveLink
+		codeLink
+		techUsed
 */
 export function ProjectObject(): IProjectObject[] {
 	const tech: ITechnologiesObject = TechnologiesObject(35);
@@ -37,65 +35,31 @@ export function ProjectObject(): IProjectObject[] {
 	// make sure projects are in the order you want them displayed
 	return [
 		{
-			id: 0,
-			media: covid,
-			title: "Covid-19 Tracker V2",
-			shortDesc: "Visualize global key data related to Covid-19 virus.",
+			media: Google,
+			title: "Google Search Clone",
+			shortDesc: "This is a remake of Google Search",
 			accomplishments: [
-				"Java Multi-threaded services for DB population & updating",
-				"CI/CD pipeline with GitHub Actions for static code analysis",
-				"Algorithms to shape data on FE and BE",
+				"Best searching algorithm in the world",
+				"All custom CSS in Material Design",
+				"Deployed to Heroku",
 			],
-			liveLink: "https://covid-19-tracker-v2-fe.vercel.app/",
-			codeLink: "https://github.com/bhaden94/Covid-19-Tracker-V2",
-			techUsed: [
-				tech.spring,
-				tech.java,
-				tech.react,
-				tech.typescript,
-				tech.materialui,
-				tech.mongodb,
-				tech.heroku,
-			],
+			liveLink: "https://www.google.com",
+			codeLink: "https://github.com/",
+			techUsed: [tech.react, tech.css, tech.typescript, tech.heroku],
 		},
 		{
-			id: 1,
-			media: spacex,
-			title: "SpaceX Timeline",
-			shortDesc:
-				"See milestones, past launches, and the next launch for SpaceX.",
+			media: Netflix,
+			title: "Netflix Clone",
+			shortDesc: "Bet you can't tell the difference...",
 			accomplishments: [
-				"React & Bootstrap to visualize data about SpaceX launches",
-				"Custom responsive sidebar component",
-				"One of a kind vertical timeline pagination!",
+				"React & Bootstrap",
+				"Custom responsiveness across entire site",
+				"One of a kind pagination techniques",
+				"Another cool accomplishment",
+				"Creating more accomplishments since I don't have technologies used",
+				"You will definitely get hired for this one",
 			],
-			liveLink: "https://bhaden94.github.io/spacex-app/",
-			codeLink: "https://github.com/bhaden94/spacex-app",
-			techUsed: [
-				tech.react,
-				tech.javascript,
-				tech.bootstrap,
-				tech.github,
-			],
+			liveLink: "https://www.netflix.com",
 		},
-		// {
-		// 	id: 2,
-		// 	media: spacex,
-		// 	title: "title",
-		// 	shortDesc:
-		// 		"short desc",
-		// 	accomplishments: [
-		// 		"acc 1",
-		// 		"acc 2",
-		// 	],
-		// 	liveLink: "link to live app",
-		// 	codeLink: "link to code",
-		// 	techUsed: [
-		// 		tech.react,
-		// 		tech.javascript,
-		// 		tech.bootstrap,
-		// 		tech.github,
-		// 	],
-		// },
 	];
 }
